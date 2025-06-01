c543ff2d151bf8dfbeff893c8b12dbb70772de9d
Michael <michaellambert649@gmail.com>
2025-06-01 14:50:36 +0200

    :clean:

--- PCA9547.c
+++ PCA9547.c
@@ -9,14 +9,9 @@ void PCA9547_Set_Channel(uint8_t canal)
     // X | X | X | X | B3 | B2 | B1 | B0 |
     // B3 : enable bit
     
-    //CTRL = 0xF8; // For�age � 1 de D7 � B3
-    //CTRL |= canal;
-    
-    //CTRL = canal;
-    //CTRL |=0xF8;
     I2C2_start();
-    I2C2_write(0b11100000); // Write
-    //I2C2_ACK();             // test
+    I2C2_write(0b11100000);
+
     switch(canal)
     {
         case(0):
@@ -45,8 +40,6 @@ void PCA9547_Set_Channel(uint8_t canal)
             break;    
     }
 
-    //I2C2_write(CTRL);
-    //I2C2_ACK();             // test
     I2C2_stop();
 }
 
@@ -56,11 +49,11 @@ uint8_t PCA9547_Read_Channel(void)
     uint8_t canal = 0;
     
     I2C2_start();
-    I2C2_write(0b11100001); // Write
-    I2C2_ACK();             // test
+    I2C2_write(0b11100001); 
+    I2C2_ACK();            
     
     canal = I2C2_read();
-    I2C2_NACK();             // test
+    I2C2_NACK();            
     I2C2_stop();    
     
     canal &= 0x07;
